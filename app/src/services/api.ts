b32aad1aedf05d491e407d51cf939ea76489d493
Franklin Ramos <franklinramos@outlook.com>
2024-12-01 17:01:59 -0300

    chore: add scripts

--- app/src/services/api.ts
+++ app/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://192.168.15.15:3000/api",
+  baseURL: `${process.env.EXPO_PUBLIC_API_URL}/api` || "http://192.168.15.150:3000/api",
 });
 
 export default api;
